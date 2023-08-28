eb4c17443fdb43458dbe03ae7c507abcc3218d3b
vovaf <vovafokyz@gmail.com>
2023-08-28 07:56:21 +0300

    3.2. Загрузка файлов на github [#243562]

--- src/main/java/ru/job4j/calculator/Calculator.java
+++ src/main/java/ru/job4j/calculator/Calculator.java
@@ -10,7 +10,7 @@ public class Calculator {
         int five = 5;
 
         int onePlusTwo = one + two;
-        int sixDivTwo = six / 2;
+        int sixDivTwo = six / two;
         int fiveMinusTwo = five - two;
         int fourTimeTwo = four * two;
         System.out.println(onePlusTwo);
