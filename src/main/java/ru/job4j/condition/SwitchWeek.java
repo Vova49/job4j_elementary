c1682ac540d6ab3d897b7f082943dafe3e035eaf
vovaf <vovafokyz@gmail.com>
2023-09-07 15:31:54 +0300

    Загрузка файлов на github

--- src/main/java/ru/job4j/condition/SwitchWeek.java
+++ src/main/java/ru/job4j/condition/SwitchWeek.java
@@ -6,20 +6,28 @@ public class SwitchWeek {
         switch (day) {
             case 1:
                 name = "Понедельник";
+                break;
             case 2:
                 name = "Вторник";
+                break;
             case 3:
                 name = "Среда";
+                break;
             case 4:
                 name = "Четверг";
+                break;
             case 5:
                 name = "Пятница";
+                break;
             case 6:
                 name = "Субота";
+                break;
             case 7:
                 name = "Воскресенье";
+                break;
             default:
                 name = "Ошибка";
+                break;
         }
         return name;
     }
