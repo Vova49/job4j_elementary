1a88fc2606d69d1ac6abc923be230eaf423158fa
vovaf <vovafokyz@gmail.com>
2023-09-12 11:09:17 +0300

    Загрузка файлов на github

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -8,5 +8,14 @@ public class ArrayDefinition {
         System.out.println(surnames.length);
         float[] prices = new float[40];
         System.out.println(prices.length);
+        String[] names = new String[4];
+        names[0] = "Petr Arsentev";
+        names[1] = "Vova Ivanov";
+        names[2] = "Stepan Korobko";
+        names[3] = "Sergey Vechkitov";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
