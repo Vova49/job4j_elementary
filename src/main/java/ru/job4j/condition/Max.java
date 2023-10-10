e7b17ca6d85912320ddb4b8f11e0b1345260ec58
vovaf <vovafokyz@gmail.com>
2023-10-10 10:40:29 +0300

    Загрузка файлов на github

--- src/main/java/ru/job4j/condition/Max.java
+++ src/main/java/ru/job4j/condition/Max.java
@@ -6,24 +6,10 @@ public class Max {
     }
 
     public static int max(int left, int right, int third) {
-        if (left > right && left > third) {
-            return left;
-        } else if (right > left && right > third) {
-            return right;
-        } else {
-            return third;
-        }
+        return max(max(left, right), third);
     }
 
     public static int max(int left, int right, int third, int four) {
-        if (left > right && left > third && left > four) {
-            return left;
-        } else if (right > left && right > third && right > four) {
-            return right;
-        } else if (third > left && third > right && third > four) {
-            return third;
-        } else {
-            return four;
-        }
+        return max(max(left, right, third), four);
     }
 }
